7653711d243b54dcc5a911ad357cdecc4dda8f53
Yura95st <yura95st@gmail.com>
2015-04-24 23:54:36 +0300

    Add NumberUtils class.

---
+++ src/floating_point_number_converter/Utils/NumberUtils.java
@@ -0,0 +1,172 @@
+package floating_point_number_converter.Utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import floating_point_number_converter.Models.FloatingPointNumber;
+
+public class NumberUtils
+{
+	/**
+	 * Convert absolute fractional part of the specified number to it's binary
+	 * representation.
+	 *
+	 * @param number
+	 *            the number
+	 * @param bitsLength
+	 *            the bits length
+	 * @return the list
+	 */
+	public static List<Boolean> convertFractionalPartToBinaryNumber(
+		double number, int bitsLength)
+		{
+		Guard.moreThanZero(bitsLength, "bitsLength");
+
+		double fractionalPart = Math.abs(NumberUtils.getFractionalPart(number));
+
+		List<Boolean> binaryNumber = new ArrayList<Boolean>();
+
+		for (int i = 0; i < bitsLength; i++)
+		{
+			fractionalPart *= 2;
+
+			boolean bitValue = (fractionalPart >= 1);
+
+			binaryNumber.add(bitValue);
+
+			fractionalPart = NumberUtils.getFractionalPart(fractionalPart);
+		}
+
+		return binaryNumber;
+		}
+
+	/**
+	 * Convert the specified number to it's binary representation.
+	 *
+	 * @param number
+	 *            the number
+	 * @param bitsLength
+	 *            the bits' length
+	 * @return the list
+	 */
+	public static List<Boolean> convertToBinaryNumber(int number, int bitsLength)
+	{
+		Guard.notNegative(number, "number");
+		Guard.moreThanZero(bitsLength, "bitsLength");
+
+		List<Boolean> binaryNumber = new ArrayList<Boolean>();
+
+		while (number != 0)
+		{
+			boolean bitValue = (number % 2 != 0);
+
+			binaryNumber.add(bitValue);
+
+			number /= 2;
+		}
+
+		for (int i = binaryNumber.size(); i < bitsLength; i++)
+		{
+			binaryNumber.add(false);
+		}
+
+		Collections.reverse(binaryNumber);
+
+		binaryNumber = binaryNumber.subList(0, bitsLength);
+
+		return binaryNumber;
+	}
+
+	/**
+	 * Convert specified number to it's floating point representation according
+	 * to the IEEE 754 standard.
+	 *
+	 * @param number
+	 *            the number
+	 * @param exponentLength
+	 *            the exponent length
+	 * @param mantissaLength
+	 *            the mantissa length
+	 * @return the floating point number
+	 */
+	public static FloatingPointNumber convertToFloatingPointNumber(
+		double number, int exponentLength, int mantissaLength)
+	{
+		Guard.moreThanZero(exponentLength, "exponentLength");
+		Guard.moreThanZero(mantissaLength, "mantissaLength");
+
+		FloatingPointNumber floatingPointNumber =
+				new FloatingPointNumber(exponentLength, mantissaLength);
+
+		boolean sign = number < 0;
+
+		floatingPointNumber.setSign(sign);
+
+		number = Math.abs(number);
+
+		int exponent = (int) Math.pow(2.0, exponentLength - 1) - 1;
+
+		// Normalize number
+		while (number >= 2 || (number > 0 && number < 1))
+		{
+			if (number >= 2)
+			{
+				number /= 2.0;
+				exponent++;
+			}
+			else if (number < 1)
+			{
+				number *= 2.0;
+
+				// Prevents overflowing
+				if (exponent > 0)
+				{
+					exponent--;
+				}
+			}
+		}
+
+		List<Boolean> exponentBinary =
+				NumberUtils.convertToBinaryNumber(exponent, exponentLength);
+
+		double fractionalPart = NumberUtils.getFractionalPart(number);
+
+		List<Boolean> fractionalPartBinary =
+				NumberUtils.convertFractionalPartToBinaryNumber(fractionalPart,
+					mantissaLength);
+
+		floatingPointNumber.setExponent(exponentBinary);
+		floatingPointNumber.setMantissa(fractionalPartBinary);
+
+		return floatingPointNumber;
+	}
+
+	/**
+	 * Gets the fractional part of the specified number.
+	 *
+	 * @param number
+	 *            the number
+	 * @return the fractional part
+	 */
+	public static double getFractionalPart(double number)
+	{
+		double fractionalPart = number - NumberUtils.getIntegerPart(number);
+
+		return fractionalPart;
+	}
+
+	/**
+	 * Gets the integer part of the specified number.
+	 *
+	 * @param number
+	 *            the number
+	 * @return the integer part
+	 */
+	public static int getIntegerPart(double number)
+	{
+		int integerPart = (int) number;
+
+		return integerPart;
+	}
+}
---
+++ test/floating_point_number_converter/Utils/NumberUtilsTests.java
@@ -0,0 +1,437 @@
+package floating_point_number_converter.Utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import floating_point_number_converter.Models.FloatingPointNumber;
+
+public class NumberUtilsTests
+{
+	@Test(expected = IllegalArgumentException.class)
+	public void convertFractionalPartToBinaryNumber_BitsLengthIsEqualToZero_ThrowsIllegalArgumentException()
+	{
+		// Arrange
+		double number = 1.0;
+
+		// Act & Assert
+		NumberUtils.convertFractionalPartToBinaryNumber(number, 0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void convertFractionalPartToBinaryNumber_BitsLengthIsNegative_ThrowsIllegalArgumentException()
+	{
+		// Arrange
+		double number = 1.0;
+
+		// Act & Assert
+		NumberUtils.convertFractionalPartToBinaryNumber(number, -1);
+	}
+
+	@Test
+	public void convertFractionalPartToBinaryNumber_NumberFractionalPartDoesNotFitInTheBitsLength_CutsExtraBits()
+	{
+		// Arrange
+		double number = 4.6;
+
+		List<Boolean> testBinaryNumber =
+			Arrays.asList(true, false, false, true, true, false);
+
+		// Act
+		List<Boolean> binaryNumber =
+			NumberUtils.convertFractionalPartToBinaryNumber(number,
+				testBinaryNumber.size());
+
+		// Assert
+		Assert.assertEquals(testBinaryNumber, binaryNumber);
+	}
+
+	@Test
+	public void convertFractionalPartToBinaryNumber_NumberFractionalPartIsEqualToZero_ReturnsValidBinaryNumber()
+	{
+		// Arrange
+		double number = 4.0;
+
+		List<Boolean> testBinaryNumber = Arrays.asList(false, false);
+
+		// Act
+		List<Boolean> binaryNumber =
+			NumberUtils.convertFractionalPartToBinaryNumber(number,
+				testBinaryNumber.size());
+
+		// Assert
+		Assert.assertEquals(testBinaryNumber, binaryNumber);
+	}
+
+	@Test
+	public void convertFractionalPartToBinaryNumber_NumberIsNegative_ReturnsValidBinaryNumber()
+	{
+		// Arrange
+		double number = -4.25;
+
+		List<Boolean> testBinaryNumber =
+			Arrays.asList(false, true, false, false);
+
+		// Act
+		List<Boolean> binaryNumber =
+			NumberUtils.convertFractionalPartToBinaryNumber(number,
+				testBinaryNumber.size());
+
+		// Assert
+		Assert.assertEquals(testBinaryNumber, binaryNumber);
+	}
+
+	@Test
+	public void convertFractionalPartToBinaryNumber_NumberIsPositive_ReturnsValidBinaryNumber()
+	{
+		// Arrange
+		double number = 4.25;
+
+		List<Boolean> testBinaryNumber =
+			Arrays.asList(false, true, false, false);
+
+		// Act
+		List<Boolean> binaryNumber =
+			NumberUtils.convertFractionalPartToBinaryNumber(number,
+				testBinaryNumber.size());
+
+		// Assert
+		Assert.assertEquals(testBinaryNumber, binaryNumber);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void convertToBinaryNumber_BitsLengthIsEqualToZero_ThrowsIllegalArgumentException()
+	{
+		// Arrange
+		int number = 1;
+
+		// Act & Assert
+		NumberUtils.convertToBinaryNumber(number, 0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void convertToBinaryNumber_BitsLengthIsNegative_ThrowsIllegalArgumentException()
+	{
+		// Arrange
+		int number = 1;
+
+		// Act & Assert
+		NumberUtils.convertToBinaryNumber(number, -1);
+	}
+
+	@Test
+	public void convertToBinaryNumber_NumberDoesNotFitInTheBitsLength_CutsExtraBits()
+	{
+		// Arrange
+		int number = 4;
+
+		List<Boolean> testBinaryNumber = Arrays.asList(true, false);
+
+		// Act
+		List<Boolean> binaryNumber =
+			NumberUtils.convertToBinaryNumber(number, testBinaryNumber.size());
+
+		// Assert
+		Assert.assertEquals(testBinaryNumber, binaryNumber);
+	}
+
+	@Test
+	public void convertToBinaryNumber_NumberIsEqualToZero_ReturnsValidBinaryNumber()
+	{
+		// Arrange
+		int number = 0;
+
+		List<Boolean> testBinaryNumber = Arrays.asList(false, false, false);
+
+		// Act
+		List<Boolean> binaryNumber =
+			NumberUtils.convertToBinaryNumber(number, testBinaryNumber.size());
+
+		// Assert
+		Assert.assertEquals(testBinaryNumber, binaryNumber);
+	}
+
+	@Test
+	public void convertToBinaryNumber_NumberIsGreaterThanZero_ReturnsValidBinaryNumber()
+	{
+		// Arrange
+		int number = 3;
+
+		List<Boolean> testBinaryNumber = Arrays.asList(true, true);
+
+		// Act
+		List<Boolean> binaryNumber =
+			NumberUtils.convertToBinaryNumber(number, testBinaryNumber.size());
+
+		// Assert
+		Assert.assertEquals(testBinaryNumber, binaryNumber);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void convertToBinaryNumber_NumberIsLessThanZero_ThrowsIllegalArgumentException()
+	{
+		// Arrange
+		int bitsLength = 1;
+
+		// Act & Assert
+		NumberUtils.convertToBinaryNumber(-1, bitsLength);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void convertToFloatingPointNumber_ExponentLengthIsEqualToZero_ThrowsIllegalArgumentException()
+	{
+		// Arrange
+		double number = 1.0;
+		int mantissaLength = 1;
+
+		// Act & Assert
+		NumberUtils.convertToFloatingPointNumber(number, 0, mantissaLength);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void convertToFloatingPointNumber_ExponentLengthIsLessThanZero_ThrowsIllegalArgumentException()
+	{
+		// Arrange
+		double number = 1.0;
+		int mantissaLength = 1;
+
+		// Act & Assert
+		NumberUtils.convertToFloatingPointNumber(number, -1, mantissaLength);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void convertToFloatingPointNumber_MantissaLengthIsEqualToZero_ThrowsIllegalArgumentException()
+	{
+		// Arrange
+		double number = 1.0;
+		int exponentLength = 1;
+
+		// Act & Assert
+		NumberUtils.convertToFloatingPointNumber(number, exponentLength, 0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void convertToFloatingPointNumber_MantissaLengthIsLessThanZero_ThrowsIllegalArgumentException()
+	{
+		// Arrange
+		double number = 1.0;
+		int exponentLength = 1;
+
+		// Act & Assert
+		NumberUtils.convertToFloatingPointNumber(number, exponentLength, -1);
+	}
+
+	@Test
+	public void convertToFloatingPointNumber_NumberExponentDoesNotFitInMantissaLength_CutsExtraBits()
+	{
+		// Arrange
+		double number = 0.5;
+		int exponentLength = 1;
+		int mantissaLength = 2;
+
+		FloatingPointNumber testFloatingPointNumber =
+			new FloatingPointNumber(exponentLength, mantissaLength);
+
+		testFloatingPointNumber.setExponent(Arrays.asList(false));
+		testFloatingPointNumber.setMantissa(Arrays.asList(false, false));
+		testFloatingPointNumber.setSign(false);
+
+		// Act
+		FloatingPointNumber floatingPointNumber =
+			NumberUtils.convertToFloatingPointNumber(number, exponentLength,
+				mantissaLength);
+
+		// Assert
+		Assert.assertEquals(testFloatingPointNumber, floatingPointNumber);
+	}
+
+	@Test
+	public void convertToFloatingPointNumber_NumberIsNegative_ReturnsValidFloatingPointNumber()
+	{
+		// Arrange
+		double number = -2.25;
+		int exponentLength = 2;
+		int mantissaLength = 4;
+
+		FloatingPointNumber testFloatingPointNumber =
+			new FloatingPointNumber(exponentLength, mantissaLength);
+
+		testFloatingPointNumber.setExponent(Arrays.asList(true, false));
+		testFloatingPointNumber.setMantissa(Arrays.asList(false, false, true,
+			false));
+		testFloatingPointNumber.setSign(true);
+
+		// Act
+		FloatingPointNumber floatingPointNumber =
+			NumberUtils.convertToFloatingPointNumber(number, exponentLength,
+				mantissaLength);
+
+		// Assert
+		Assert.assertEquals(testFloatingPointNumber, floatingPointNumber);
+	}
+
+	@Test
+	public void convertToFloatingPointNumber_NumberIsPositive_ReturnsValidFloatingPointNumber()
+	{
+		// Arrange
+		double number = 2.25;
+		int exponentLength = 2;
+		int mantissaLength = 4;
+
+		FloatingPointNumber testFloatingPointNumber =
+			new FloatingPointNumber(exponentLength, mantissaLength);
+
+		testFloatingPointNumber.setExponent(Arrays.asList(true, false));
+		testFloatingPointNumber.setMantissa(Arrays.asList(false, false, true,
+			false));
+		testFloatingPointNumber.setSign(false);
+
+		// Act
+		FloatingPointNumber floatingPointNumber =
+			NumberUtils.convertToFloatingPointNumber(number, exponentLength,
+				mantissaLength);
+
+		// Assert
+		Assert.assertEquals(testFloatingPointNumber, floatingPointNumber);
+	}
+
+	@Test
+	public void convertToFloatingPointNumber_NumberMantissaDoesNotFitInMantissaLength_CutsExtraBits()
+	{
+		// Arrange
+		double number = 0.3;
+		int exponentLength = 4;
+		int mantissaLength = 4;
+
+		FloatingPointNumber testFloatingPointNumber =
+			new FloatingPointNumber(exponentLength, mantissaLength);
+
+		testFloatingPointNumber.setExponent(Arrays.asList(false, true, false,
+			true));
+		testFloatingPointNumber.setMantissa(Arrays.asList(false, false, true,
+			true, false, false, true));
+		testFloatingPointNumber.setSign(false);
+
+		// Act
+		FloatingPointNumber floatingPointNumber =
+			NumberUtils.convertToFloatingPointNumber(number, exponentLength,
+				mantissaLength);
+
+		// Assert
+		Assert.assertEquals(testFloatingPointNumber, floatingPointNumber);
+	}
+
+	@Test
+	public void getFractionalPart_NumberFractionalPartIsEqualToZero_ReturnsZero()
+	{
+		// Arrange
+		double testFractionalPart = 0.0;
+		double number = 1 + testFractionalPart;
+
+		// Act
+		double fractionalPart = NumberUtils.getFractionalPart(number);
+
+		// Assert
+		Assert.assertEquals(testFractionalPart, fractionalPart, 1e-64);
+	}
+
+	@Test
+	public void getFractionalPart_NumberIsNegative_ReturnsValidFractionalPart()
+	{
+		// Arrange
+		double testFractionalPart = -0.9876;
+		double number = -1 + testFractionalPart;
+
+		// Act
+		double fractionalPart = NumberUtils.getFractionalPart(number);
+
+		// Assert
+		Assert.assertEquals(testFractionalPart, fractionalPart, 1e-64);
+	}
+
+	@Test
+	public void getFractionalPart_NumberIsPositive_ReturnsValidFractionalPart()
+	{
+		// Arrange
+		double testFractionalPart = 0.9876;
+		double number = 1 + testFractionalPart;
+
+		// Act
+		double fractionalPart = NumberUtils.getFractionalPart(number);
+
+		// Assert
+		Assert.assertEquals(testFractionalPart, fractionalPart, 1e-64);
+	}
+
+	@Test
+	public void getFractionalPart_NumberIsZero_ReturnsZero()
+	{
+		// Arrange
+		double testFractionalPart = 0.0;
+		double number = 0.0;
+
+		// Act
+		double fractionalPart = NumberUtils.getFractionalPart(number);
+
+		// Assert
+		Assert.assertEquals(testFractionalPart, fractionalPart, 1e-64);
+	}
+
+	@Test
+	public void getIntegerPart_NumberIsNegative_ReturnsValidIntegerPart()
+	{
+		// Arrange
+		int testIntegerPart = -1;
+		double number = testIntegerPart - 0.9876;
+
+		// Act
+		int integerPart = NumberUtils.getIntegerPart(number);
+
+		// Assert
+		Assert.assertEquals(testIntegerPart, integerPart);
+	}
+
+	@Test
+	public void getIntegerPart_NumberIsNegativeAndGreaterThanMinusOne_ReturnsZero()
+	{
+		// Arrange
+		int testIntegerPart = 0;
+		double number = -0.9876;
+
+		// Act
+		int integerPart = NumberUtils.getIntegerPart(number);
+
+		// Assert
+		Assert.assertEquals(testIntegerPart, integerPart);
+	}
+
+	@Test
+	public void getIntegerPart_NumberIsPositive_ReturnsValidIntegerPart()
+	{
+		// Arrange
+		int testIntegerPart = 1;
+		double number = testIntegerPart + 0.9876;
+
+		// Act
+		int integerPart = NumberUtils.getIntegerPart(number);
+
+		// Assert
+		Assert.assertEquals(testIntegerPart, integerPart);
+	}
+
+	@Test
+	public void getIntegerPart_NumberIsPositiveAndLessThanOne_ReturnsZero()
+	{
+		// Arrange
+		int testIntegerPart = 0;
+		double number = 0.9876;
+
+		// Act
+		int integerPart = NumberUtils.getIntegerPart(number);
+
+		// Assert
+		Assert.assertEquals(testIntegerPart, integerPart);
+	}
+}
